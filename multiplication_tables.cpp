45f211411315d33c60b60388b619c43e6edebe05
Mimifan <mimifan.dev@gmail.com>
2022-09-07 11:39:17 +0200

    User input added
    Now the multiplication table creation isn't limited anymore to 25 and 50.
    The user of the program can now input it's language and number of tables/values

--- multiplication_tables.cpp
+++ multiplication_tables.cpp
@@ -2,16 +2,57 @@
 #include <fstream>
 #include <string>
 #include <direct.h>
+#include<windows.h>
+
 using namespace std;
+
+string lang;
+int tables;
+int max_value;
+
+void defineValues(string language){
+    if(language == "en") {
+        cout << "How many multiplication tables do you want ?" << endl;
+        cin >> tables;
+        cout << "How many values multiplicated do you want ?" << endl;
+        cin >> max_value;
+    } else if(language == "fr") {
+        cout << "Combien de tables voulez vous creer ?" << endl;
+        cin >> tables;
+        cout << "Combien de valeurs multipliees voulez-vous ?" << endl;
+        cin >> max_value;
+    } else {
+        cout << "Unknown languge. Please restart program." << endl;
+        cout << "Program will exit in 5s" << endl;
+        Sleep(1000);
+        cout << "Program will exit in 4s" << endl;
+        Sleep(1000);
+        cout << "Program will exit in 3s" << endl;
+        Sleep(1000);
+        cout << "Program will exit in 2s" << endl;
+        Sleep(1000);
+        cout << "Program will exit in 1s" << endl;
+        Sleep(1000);
+        exit(3);
+        return;
+    }
+}
+
 int main(){
-    int i = 0;
-    int t = 0;
     ofstream tfile;
+
+    cout << "Language ? (en/fr)" << endl;
+    cin >> lang;
+    defineValues(lang);
+
+    int i = 1;
+    int t = 1;
+
     _mkdir(".\\Tables");
     string fileName = ".txt";
-    while(t <= 25){
+    while(t <= tables){
         tfile.open(".\\Tables\\" + to_string(t) + fileName);
-        while (i <= 50) {
+        while (i <= max_value) {
             tfile << t << "*" << i << " = " << t*i << endl;
             i++;
         }
@@ -22,3 +63,4 @@ int main(){
     cout << "tout a fonctionné sans soucis." << endl;
     return 0;
 }
+
